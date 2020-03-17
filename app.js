6480fe68adf8dd455dc22d80b519400a02c5626b
kylejvh <49110126+kylejvh@users.noreply.github.com>
2020-03-16 20:02:17 -0400

    Deployment changes

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ console.log(__dirname);
 // Server static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
