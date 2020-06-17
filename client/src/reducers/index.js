62734b8fde19e2167677620dc31abe2a49fd87d7
kylejvh <49110126+kylejvh@users.noreply.github.com>
2020-06-17 19:33:17 -0400

    Fixed logout bug: state is now fully reset as intended

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,21 @@
 import { combineReducers } from "redux";
+import { LOGOUT_SUCCEEDED } from "../actions/types";
 import auth from "./auth";
 import chatrooms from "./chatrooms";
 import message from "./message";
 import notify from "./notify";
 
-export default combineReducers({
+const appReducer = combineReducers({
   auth,
   chatrooms,
   message,
   notify,
 });
+
+export default (state, action) => {
+  if (action.type === LOGOUT_SUCCEEDED) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
